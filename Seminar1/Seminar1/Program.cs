a82a8eab8910d50e7c70c73b3df38a20f923f722
Serban Timofte <serbantimofte@Serbans-MacBook-Air.local>
2024-02-20 12:54:19 +0200

    Seminar 1 20.02

---
+++ Seminar1/Seminar1/Program.cs
@@ -0,0 +1,44 @@
+﻿//Ad References before using into the project
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TOATE CLASELE MOSTENESC OBJECT (si are metoda to_string pe care o apeleaza in Console.WriteLine(a1_bisu); - ToString(a1_bisu) )
+            Animal a1_bisu = new Animal(4, "Bisu", 11);
+            Console.WriteLine(a1_bisu);
+
+            Animal a2_simba = new Animal(6, "Simba", 5);
+            Console.WriteLine(a2_simba);
+            Console.WriteLine();
+
+            Animal a3 = a1_bisu.Clone();
+
+            Urs martinel = new Urs(true, 10, "Martinel", 150);
+
+            List<Animal> pets = new List<Animal>();
+            pets.Add(a1_bisu);
+            pets.Add(a2_simba);
+            pets.Add(a3);
+            pets.Add(martinel);
+
+            a1_bisu.Nume = "Bus";
+
+            pets.Sort();
+
+            foreach (Animal a in pets)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Seminar1/Seminar1/Urs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar1
+{
+    internal class Urs : Animal
+    {
+        private bool hiberneaza;
+
+        public bool Hiberneaza
+        {
+            get { return this.hiberneaza; }
+            set { if(this.hiberneaza != value) this.hiberneaza = value; }
+        }
+
+        public Urs(bool h, int v, string n, float g) : base(v, n, g)
+        {
+            this.hiberneaza = h;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " si " + (hiberneaza? "hiberneaza" : "nu hiberneaza!");
+        }
+    }
+}
